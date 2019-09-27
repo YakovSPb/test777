4018cc1aaa8a042805865231a342cecee7ed8c01
YakovSPb <yakov_dux@bk.ru>
2019-09-27 12:40:11 +0300

    Job is done

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,16 @@
 $(function() {
 
-	// Custom JS
+        var link = $('.gumburger');
+        var close = $('.m-menu__button--close');
+        var menu = $('.m-menu');
+        link.on('click', function(event){
+            event.preventDefault();
+            menu.toggleClass('m-menu__active')
+        });
+        close.on('click', function(event){
+            event.preventDefault();
+            menu.toggleClass('m-menu__active')
+        });
+        
 
 });
